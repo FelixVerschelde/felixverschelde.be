c0e8981827f8969a374a1c18ed526c4cba816635
felix.verschelde <felix.verschelde@student.howest.be>
2022-05-26 18:30:28 +0200

    first commit

---
+++ assets/js/commands.js
@@ -0,0 +1,24 @@
+const helpArray = [
+    '<p class="help">About:</p>          <p class="discription">About me.</p>',
+    '<p class="help">Projects:</p>       <p class="discription">What I have made.</p>',
+    '<p class="help">Socials:</p>        <p class="discription">Links to socials.</p>',
+    '<p class="help">Clear:</p>          <p class="discription">This clears the page of all previous commands.</p>',
+]
+
+const aboutMe = [
+    "<p class=discription>Hey, my name is Felix,</p>",
+    "<p class=discription>I'm a first year student at Howest studying applied computer science.</p>",
+    "<p class=discription>I'm currently working on this project in my spare time,</p>",
+    "<p class=discription>but I'm also working on other things.</p>",
+]
+
+const myProjects = [
+    "<p class=discription>You can find all my projects where I worked on so far here: </p>",
+    "<a href=https://github.com/FelixVerschelde target=blank>Github</a>"
+]
+
+const mySocials = [
+    "<p>You can find all my socials here: </p>",
+    "<a href=https://www.linkedin.com/in/felixverschelde target=blank>LinkedIn</a>",
+    "<a href=https://www.facebook.com/profile.php?id=100012648430952 target=blank>Facebook</a>",
+]
---
+++ assets/js/script.js
@@ -0,0 +1,108 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    const searchbar = document.querySelector('#searchbar');
+    searchbar.addEventListener('keydown', e => {
+        const enterKeyCode = 13;
+        const enterKey = e.keyCode === enterKeyCode;
+        if (enterKey) {
+            checkCommand(e);
+        } else {
+            return false;
+        }
+    });
+}
+
+
+function checkCommand(e) {
+    e.preventDefault();
+    command(e.target.value);
+}
+
+
+
+function help() {
+    const textarea = document.querySelector("textarea");
+    const $ul = document.querySelector("ul");
+    $ul.innerHTML = "";
+    let $html = "";
+
+    helpArray.forEach(element => {
+        $html += `<li>${element}</li>`;
+    });
+
+    $ul.insertAdjacentHTML('beforeend', $html);
+    textarea.value = "";
+}
+
+function about() {
+    const textarea = document.querySelector("textarea");
+    const $div = document.querySelector("div");
+    let $html = "";
+
+    aboutMe.forEach(element => {
+        $html += `<p>${element}</p>`;
+    });
+
+    $div.insertAdjacentHTML('beforeend', $html);
+    textarea.value = "";
+}
+
+function projects() {
+    const textarea = document.querySelector("textarea");
+    const $div = document.querySelector("div");
+    let $html = "";
+
+    myProjects.forEach(element => {
+        $html += `<p>${element}</p>`;
+    });
+
+    $div.insertAdjacentHTML('beforeend', $html);
+    textarea.value = "";
+}
+
+function socials() {
+    const textarea = document.querySelector("textarea");
+    const $div = document.querySelector("div");
+    let $html = "";
+
+    mySocials.forEach(element => {
+        $html += `<p>${element}</p>`;
+    });
+
+    $div.insertAdjacentHTML('beforeend', $html);
+    textarea.value = "";
+}
+
+function clear() {
+    const textarea = document.querySelector("textarea");
+    const $div = document.querySelector("div");
+    const $ul = document.querySelector("ul");
+    $ul.innerHTML = "";
+    $div.innerHTML = "";
+    textarea.value = "";
+}
+
+function command(cmd) {
+    switch (cmd.toLowerCase()) {
+        case 'help':
+            help();
+            break;
+        case 'about':
+            about();
+            break;
+        case 'projects':
+            projects();
+            break;
+        case 'socials':
+            socials();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default:
+            console.log('Command not found');
+    }
+}
